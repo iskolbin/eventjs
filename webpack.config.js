e4d1e439795204c1f84516e976c7007850b4f6cc
Ilya Kolbin <iskolbin@gmail.com>
2017-03-13 14:53:08 +0300

    upd

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+	entry: './src/index.js',
+	devtool: 'source-map',
+	output: {
+		path: './dist',
+		filename: 'EVENT.min.js',
+	},
+	plugins: [
+		new webpack.optimize.UglifyJsPlugin({minimize: true})
+	],
+	module: {
+		loaders: [
+			{
+
+				loader: "babel-loader",
+
+				// Skip any files outside of your project's `src` directory
+				include: [
+					path.resolve(__dirname, "src"),
+				],
+
+
+				// Only run `.js` and `.jsx` files through Babel
+				test: /\.jsx?$/,
+
+				// Options to configure babel with
+				query: {
+					plugins: ['transform-runtime'],
+					presets: ['es2015'],
+				}
+			},
+		]
+	}
+}
